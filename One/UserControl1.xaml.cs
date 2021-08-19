34307a69b7a539b9753f788eb31042dbb5862f50
Jarl Haggerty <jarl@liquidsky.com>
2021-08-19 16:18:06 -0400

    First

---
+++ One/UserControl1.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace One
+{
+    using System.Windows.Input;
+    using System.Windows.Controls;
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void innerRect_MouseDownOne(object sender, MouseButtonEventArgs e)
+        {
+        }
+    }
+}
---
+++ Two/UserControl1.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace Two
+{
+    using System.Windows.Input;
+    using System.Windows.Controls;
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void innerRect_MouseDownTwo(object sender, MouseButtonEventArgs e)
+        {
+        }
+    }
+}
